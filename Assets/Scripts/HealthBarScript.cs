6d886140c0afad20d9f7a25ae396a6511a13bcec
Eduardo Peña <edu.p.royuela@gmail.com>
2020-07-20 19:56:58 +0200

    HealthBar

---
+++ Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+	public Slider slider;
+	public Gradient gradient;
+	public Image fill;
+
+	public void SetMaxHealth(int health) {
+		slider.maxValue = health;
+		slider.value = health;
+
+		//Pongo el color de maxHealth.
+		fill.color = gradient.Evaluate(1);
+	}
+
+	public void setHealth(int health) {
+		slider.value = health;
+		//No esta funcionando, pero en teoría la vida debería cambiar de color.
+		fill.color = gradient.Evaluate(slider.normalizedValue);
+	}
+}
---
+++ Assets/Scripts/TakeDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamage : MonoBehaviour
+{
+	public int maxHealth = 100;
+	public int currentHealth;
+	//Da error cuando descomento esto.
+	//public HealthBar healthBar;
+
+	void Start() {
+		currentHealth = maxHealth;
+		//healthBar.SetMaxHealth(maxHealth);
+	}
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			Damage(20);
+		}
+	}
+
+	void Damage(int damage) {
+		currentHealth -= damage;
+		//healthBar.setHealth(currentHealth);
+	}
+}
