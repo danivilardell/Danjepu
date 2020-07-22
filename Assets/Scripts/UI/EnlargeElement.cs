dff08d582018ac545d8da025dd2c013688d59c70
Dani Vilardell <danivilardell@gmail.com>
2020-07-22 16:56:20 +0200

    dani menu

---
+++ Assets/Scripts/UI/EnlargeElement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnlargeElement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+ 
+     Vector3 cachedScale;
+ 
+     void Start() {
+ 
+         cachedScale = transform.localScale;
+     }
+ 
+     public void OnPointerEnter(PointerEventData eventData) {
+ 
+         transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+     }
+ 
+     public void OnPointerExit(PointerEventData eventData) {
+ 
+         transform.localScale = cachedScale;
+     }
+ }
