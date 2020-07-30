790f3c33dc99f4a97f8e31931ca86a3929b33767
Josep McNally <josep.fontanamcnally@gmail.com>
2020-07-31 00:15:12 +0200

    res

---
+++ Assets/Scripts/Funcions/DrawFunction.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using B83.ExpressionParser;
+
+public class DrawFunction : MonoBehaviour
+{
+    GameObject graph;
+
+    public List<Vector2> points;
+
+    private EdgeCollider2D col;
+    private LineRenderer line;
+    private GameObject inputText;
+    //private var parser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        graph = gameObject;
+        col = graph.transform.GetComponentInChildren<EdgeCollider2D>();
+        line = graph.GetComponentInChildren<LineRenderer>();
+        inputText = graph.transform.Find("Canvas").Find("InputField").Find("Text").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.Space)) 
+        // {
+        //     FuncToPoints();
+        //     col.points = points.ToArray();
+        //     int j = points.Count;
+        //     line.positionCount = j;
+        //     for (int i = 0; i < j; ++i) line.SetPosition(i, points[i]);
+        // }
+    }
+
+    void FuncToPoints(string f) 
+    {
+        var parser = new ExpressionParser();
+        Expression exp = parser.EvaluateExpression(f);
+        // Expression exp = parser.EvaluateExpression("sin(x*PI/180)");
+
+        points = new List<Vector2>();
+        // ho escalo per que aixi no depen del "scale" de la grafica
+        float xscale = graph.transform.localScale[0];
+        float yscale = graph.transform.localScale[1];
+        
+        for (float x = -1f; x <= 1.01f; x += .01f) {
+            exp.Parameters["x"].Value = x; // set the named parameter "x"
+            points.Add( new Vector2(x/xscale, (float) (exp.Value)/yscale) );
+            //points.Add( new Vector2(i/xscale, (i*i)/yscale) );
+        }
+    }
+
+    public void NewFunction() 
+    {
+        string function = inputText.GetComponent<Text>().text;
+        Debug.Log(function);
+        FuncToPoints(function);
+        
+        // amb el collider simplement es pot fer aixo
+        col.points = points.ToArray();
+        // amb el line renderer, com que es 3D sembla que s'ha de fer aquest arreglo
+        int j = points.Count;
+        line.positionCount = j;
+        for (int i = 0; i < j; ++i) line.SetPosition(i, points[i]);
+    }
+}
---
+++ Assets/Scripts/Funcions/ExpressionsExamples.cs
@@ -0,0 +1,53 @@
+﻿/*
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+!!! necessari !!!
+using B83.ExpressionParser;
+!!! necessari !!!
+
+// la pagina web on ho he trobat
+// http://wiki.unity3d.com/index.php/ExpressionParser
+
+var parser = new ExpressionParser();
+Expression exp = parser.EvaluateExpression("(5+3)*8^2-5*(-2)");
+Debug.Log("Result: " + exp.Value);  // prints: "Result: 522"
+ 
+Debug.Log("Result: " + parser.Evaluate("ln(e^5)"));  // prints: "Result: 5"
+ 
+ 
+// unknown identifiers are simply translated into parameters:
+ 
+Expression exp2 = parser.EvaluateExpression("sin(x*PI/180)");
+exp2.Parameters["x"].Value = 45; // set the named parameter "x"
+Debug.Log("sin(45°): " + exp2.Value); // prints "sin(45°): 0.707106781186547" 
+ 
+ 
+// convert the expression into a delegate:
+ 
+var sinFunc = exp2.ToDelegate("x");
+Debug.Log("sin(90°): " + sinFunc(90)); // prints "sin(90°): 1" 
+ 
+ 
+// It's possible to return multiple values, but it generates extra garbage for each call due to the return array:
+ 
+var exp3 = parser.EvaluateExpression("sin(angle/180*PI) * length,cos(angle/180*PI) * length");
+var f = exp3.ToMultiResultDelegate("angle", "length");
+double[] res = f(45,2);  // res contains [1.41421356237309, 1.4142135623731]
+ 
+ 
+// To add custom functions to the parser, you just have to add it before parsing an expression:
+ 
+parser.AddFunc("test", (p) => {
+    Debug.Log("TEST: " + p.Length);
+    return 42;
+});
+Debug.Log("Result: "+parser.Evaluate("2*test(1,5)")); // prints "TEST: 2" and "Result: 84"
+ 
+ 
+// NOTE: functions without parameters are not supported, use "constants" instead:
+parser.AddConst("meaningOfLife", () => 42);
+Console.WriteLine("Result: " + parser.Evaluate("2*meaningOfLife")); // prints "Result: 84"
+*/
