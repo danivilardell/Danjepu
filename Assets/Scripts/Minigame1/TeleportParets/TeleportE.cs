93a2840db4c907e94e6bbc2dcbdd6dc02d6cce1f
Josep McNally <josep.fontanamcnally@gmail.com>
2020-08-02 22:51:11 +0200

    prefabs de parets

---
+++ Assets/Scripts/Minigame1/TeleportParets/TeleportE.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportE : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     void OnCollisionEnter2D(Collision2D collision) 
+     {
+         // faria algo com collision.transform.position.x = -collision.transform.position.x
+         // pero no funciona, ho he buscat i es veu que s'ha de fer algo aixi :( es super feo.
+         Vector3 p = collision.transform.position;
+         // el -1 es per prevenir bugs de teleportacio infinita
+         p[0] = -p[0] - 1;
+         collision.transform.position = p;
+     }
+}
