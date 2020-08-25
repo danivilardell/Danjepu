45db69c241b168571e6d87759b940434c6dcc8d6
Eduardo Peña <edu.p.royuela@gmail.com>
2020-08-25 10:58:57 +0200

    Shader

---
+++ Assets/Scripts/Minigame2/Game_Control.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Game_Control : MonoBehaviour
+{
+    GameObject token;
+    List<int> faceindexes = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7};
+    public static System.Random rnd = new System.Random();
+    public int shuffleNum = 0;
+    public int[] visibleFaces = { -1, -2 };
+
+    GameObject[] list;
+    public Sprite back;
+    public GameObject first;
+
+    public int matches = 0;
+    public int TotalMatches;
+    public GameObject completeLevelUI;
+
+    public bool OptionMenu = false;
+
+    void Start()
+    {
+        int originalLength = faceindexes.Count;
+        TotalMatches = originalLength / 2;
+        float yPosition = 1.5f;
+        float xPosition = -7f;
+        for(int i = 0; i < originalLength - 1; ++i)
+        {
+            shuffleNum = rnd.Next(0, faceindexes.Count);
+            var temp = Instantiate(token, new Vector3(xPosition, yPosition, 0), Quaternion.identity);
+            temp.GetComponent<MainToken>().faceIndex = faceindexes[shuffleNum];
+            faceindexes.Remove(faceindexes[shuffleNum]);
+            xPosition = xPosition + 2;
+            //Esto solo esta preparado para 2 filas de cartas.
+            if(i == originalLength/2 - 2)
+            {
+                yPosition = -1.5f;
+                xPosition = -7f;
+            }
+        }
+        token.GetComponent<MainToken>().faceIndex = faceindexes[0];
+
+        //Cojo todos los GameObjects de la partida y los pongo en un array.
+        list = GameObject.FindGameObjectsWithTag("Carta");
+    }
+
+    public bool TwoCardsUp()
+    {
+        if(visibleFaces[0] >= 0 && visibleFaces[1] >= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void AddVisibleFace(int index)
+    {
+        if(visibleFaces[0] == -1)
+        {
+            visibleFaces[0] = index;
+        }
+
+        else if(visibleFaces[1] == -2)
+        {
+            visibleFaces[1] = index;
+        }
+    }
+
+    public void RemoveVisibleFace(int index)
+    {
+        if (visibleFaces[0] == index)
+        {
+            visibleFaces[0] = -1;
+        }
+
+        else if (visibleFaces[1] == index)
+        {
+            visibleFaces[1] = -2;
+        }
+    }
+
+    public bool CheckMatch()
+    {
+        if(visibleFaces[0] == visibleFaces[1])
+        {
+            FindObjectOfType<AudioManager>().PlaySound("Campanilla");
+            visibleFaces[0] = -1;
+            visibleFaces[1] = -2;
+            ++matches;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool NoMatch()
+    {
+        if(visibleFaces[0] >= 0 && visibleFaces[1] >= 0 && visibleFaces[0] != visibleFaces[1])
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void clean()
+    {
+        foreach(GameObject card in list)
+        {
+            if(card.GetComponent<MainToken>().matched == false)
+            {
+               card.GetComponent<SpriteRenderer>().sprite = back;
+            }
+        }
+        visibleFaces[0] = -1;
+        visibleFaces[1] = -2;
+    }
+
+    public void firstMatched()
+    {
+        first.GetComponent<MainToken>().matched = true;
+        first.GetComponent<MainToken>().isDissolving = true;
+    }
+
+    public void CompleteLevel()
+    {
+        FindObjectOfType<AudioManager>().PlaySound("Aplausos");
+
+        //Al activar "completeLevelUI" se dispara su transición y esta activa el cambio de escena al final.
+        completeLevelUI.SetActive(true); 
+
+    }
+
+    public void OptionButtonOn()
+    {
+        //Estoy en el menú de opciones. Triggered por botón.
+        OptionMenu = true;
+    }
+
+    public void OptionButtonOff()
+    {
+        //Salgo del menú de opciones. Triggered por botón.
+        OptionMenu = false;
+    }
+
+    private void Awake()
+    {
+        token = GameObject.Find("Token");
+    }
+}
---
+++ Assets/Scripts/Minigame2/MainToken.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainToken : MonoBehaviour
+{
+    GameObject gameControl;
+    SpriteRenderer spriteRenderer;
+    public Sprite[] faces;
+    public Sprite back;
+    public int faceIndex;
+    public bool matched = false;
+
+    public GameObject card;
+    public float waitingTime = 0.5f;
+
+    public Animator shake;
+
+    Material material;
+    public bool isDissolving = false;
+    float fade = 1f;
+
+    public void OnMouseDown()
+    {
+        //Si la carta ya esta emparejada o estamos en el menú de pausa las cartas no deben interactuar.
+        if(!matched && !gameControl.GetComponent<Game_Control>().OptionMenu)
+        {
+            if (spriteRenderer.sprite == back)
+            {
+                if(!gameControl.GetComponent<Game_Control>().TwoCardsUp())
+                {
+                    spriteRenderer.sprite = faces[faceIndex];
+                    gameControl.GetComponent<Game_Control>().AddVisibleFace(faceIndex);
+                    matched = gameControl.GetComponent<Game_Control>().CheckMatch();
+
+                    if (gameControl.GetComponent<Game_Control>().visibleFaces[1] == -2 && !matched)
+                    {
+                        gameControl.GetComponent<Game_Control>().first = card;
+                    }
+
+                    if(matched)
+                    {
+                        gameControl.GetComponent<Game_Control>().firstMatched();
+                        isDissolving = true;
+                    }
+                }
+
+                //Si he hecho todas las parejas he ganado el juego.
+                if(gameControl.GetComponent<Game_Control>().matches == gameControl.GetComponent<Game_Control>().TotalMatches)
+                {
+                    gameControl.GetComponent<Game_Control>().CompleteLevel();
+                }
+
+                if(gameControl.GetComponent<Game_Control>().NoMatch())
+                {
+                    //Wait y clean().
+                    StartCoroutine(Wait());
+                }
+            }
+
+            else
+            {
+                spriteRenderer.sprite = back;
+                gameControl.GetComponent<Game_Control>().RemoveVisibleFace(faceIndex);
+            }
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitingTime);
+        gameControl.GetComponent<Game_Control>().clean();
+    }
+
+    private void Update()
+    {
+        if(isDissolving)
+        {
+            fade -= Time.deltaTime;
+
+            if (fade <= 0f)
+            {
+                fade = 0f;
+                isDissolving = false;
+            }
+            //Pongo el sprite de la Shadow a null.
+            card.GetComponent<DropShadow>().shadowGameobject.GetComponent<SpriteRenderer>().sprite = null;
+            material.SetFloat("_Fade", fade);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if(!matched)
+        {
+            card.GetComponent<DropShadow>().shadowGameobject.GetComponent<SpriteRenderer>().sprite = card.GetComponent<SpriteRenderer>().sprite;
+        }
+        if (spriteRenderer.sprite == back)
+        {
+            shake.SetTrigger("Start_Shake");
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        card.GetComponent<DropShadow>().shadowGameobject.GetComponent<SpriteRenderer>().sprite = null;
+        shake.SetTrigger("Stop_Shake");
+    }
+
+    private void Awake()
+    {
+        gameControl = GameObject.Find("GameControl");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        material = GetComponent<SpriteRenderer>().material;
+    }
+}
