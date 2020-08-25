35970a00f7470e96f23b3468bdd912e07fd70709
Dani Vilardell <danivilardell@gmail.com>
2020-08-25 15:46:27 +0200

    minigame3 menu

---
+++ Assets/Scripts/Minigame3/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject levels;
+    public GameObject player;
+
+    void LoadLevel(int level) {
+    	levels.transform.GetChild(level).gameObject.SetActive(true);
+    	player.transform.position = levels.transform.GetChild(level).GetChild(0).position;
+    }
+
+}
